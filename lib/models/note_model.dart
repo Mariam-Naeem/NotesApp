ce4ba429e4e30e4434f2e4778ed243d3640e08d6
Mariam naeem saeed <mariam77naeem@gmail.com>
2025-08-10 02:04:46 +0300

    create new model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,7 @@
+class NoteModel {
+  final String title;
+  final String suntitle;
+  final String date;
+  final int color;
+  NoteModel({required this.title, required this.suntitle, required this.date, required this.color,});
+}
